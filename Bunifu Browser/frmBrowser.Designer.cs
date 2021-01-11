bd22e03a91d3deb8d33f5f995c010251bceff2a5
zz <zz@test.com>
2021-01-11 16:11:17 +0800

    Update frmBrowser.Designer.cs

--- Bunifu Browser/frmBrowser.Designer.cs
+++ Bunifu Browser/frmBrowser.Designer.cs
@@ -68,6 +68,7 @@
             this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
             this.webBrowser1.Location = new System.Drawing.Point(5, 72);
             this.webBrowser1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(27, 23);
